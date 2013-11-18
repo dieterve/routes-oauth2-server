f93d0685c241d6d4e9f8faf43e25ffef5128e2de
dieterve <dieter@dieterve.be>
2013-11-18 08:58:23 +0100

    Register PDO as a service for later use.

--- web/index.php
+++ web/index.php
@@ -5,29 +5,34 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-// providers
+// services
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
-$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__."/../config/parameters.yml"));
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__.'/../config/parameters.yml'));
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__.'/../src',
 ));
-
 $app['http_client'] = new \Guzzle\Http\Client();
+$app['database'] = new Pdo(
+	'mysql:dbname=' . $app['config']['database']['name'] . ';host=' . $app['config']['database']['hostname'],
+	$app['config']['database']['username'],
+	$app['config']['database']['password']
+);
 
 $app->before(function($request) {
 	$request->getSession()->start();
 });
 
+// routes
 $app->match('/', function() use ($app)
 {
 	// there is no index, perhaps later show documentation or perform a redirect
 	return $app->json(array('message' => 'Nothing here.'));
 });
-
 $app->mount('/authentication', new HikingRoutes\Authentication\Authentication());
 $app->mount('/myroutes', new MyRoutes\Routes\Routes());
 
+// error handling in json format
 $app->error(function(\Exception $e, $code) use ($app)
 {
 	/*
